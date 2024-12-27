a9c70d5d284fcdfbcc057d75986d6ee7f207e5c1
Hello There <quanganle83@gmail.com>
2024-12-28 00:50:05 +0700

    update

--- FormChinh/frmQuanLyTaiKhoan.Designer.cs
+++ FormChinh/frmQuanLyTaiKhoan.Designer.cs
@@ -33,17 +33,19 @@
             this.label3 = new System.Windows.Forms.Label();
             this.label4 = new System.Windows.Forms.Label();
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
-            this.MSV = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.Hoten = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.lop = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.button1 = new System.Windows.Forms.Button();
             this.btnThoat = new System.Windows.Forms.Button();
-            this.btnDoiMatKhau = new System.Windows.Forms.Button();
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.textBox2 = new System.Windows.Forms.TextBox();
             this.textBox3 = new System.Windows.Forms.TextBox();
             this.textBox4 = new System.Windows.Forms.TextBox();
             this.label5 = new System.Windows.Forms.Label();
+            this.MSV = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Hoten = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.lop = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
@@ -95,37 +97,17 @@
             this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
             this.MSV,
             this.Hoten,
-            this.lop});
-            this.dataGridView1.Location = new System.Drawing.Point(16, 187);
+            this.lop,
+            this.Column1});
+            this.dataGridView1.Location = new System.Drawing.Point(16, 193);
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.ReadOnly = true;
             this.dataGridView1.Size = new System.Drawing.Size(533, 163);
             this.dataGridView1.TabIndex = 4;
             // 
-            // MSV
-            // 
-            this.MSV.HeaderText = "MSV";
-            this.MSV.Name = "MSV";
-            this.MSV.ReadOnly = true;
-            this.MSV.Width = 130;
-            // 
-            // Hoten
-            // 
-            this.Hoten.HeaderText = "Họ Tên";
-            this.Hoten.Name = "Hoten";
-            this.Hoten.ReadOnly = true;
-            this.Hoten.Width = 180;
-            // 
-            // lop
-            // 
-            this.lop.HeaderText = "Lớp";
-            this.lop.Name = "lop";
-            this.lop.ReadOnly = true;
-            this.lop.Width = 180;
-            // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(16, 368);
+            this.button1.Location = new System.Drawing.Point(116, 368);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(76, 39);
             this.button1.TabIndex = 5;
@@ -141,16 +123,6 @@
             this.btnThoat.Text = "Thoát";
             this.btnThoat.UseVisualStyleBackColor = true;
             this.btnThoat.Click += new System.EventHandler(this.btnThoat_Click);
-            // 
-            // btnDoiMatKhau
-            // 
-            this.btnDoiMatKhau.Location = new System.Drawing.Point(328, 368);
-            this.btnDoiMatKhau.Name = "btnDoiMatKhau";
-            this.btnDoiMatKhau.Size = new System.Drawing.Size(103, 39);
-            this.btnDoiMatKhau.TabIndex = 7;
-            this.btnDoiMatKhau.Text = "Đổi Mật khẩu";
-            this.btnDoiMatKhau.UseVisualStyleBackColor = true;
-            this.btnDoiMatKhau.Click += new System.EventHandler(this.btnDoiMatKhau_Click);
             // 
             // textBox1
             // 
@@ -194,17 +166,64 @@
             this.label5.TabIndex = 12;
             this.label5.Text = "Chuyên Ngành:";
             // 
+            // MSV
+            // 
+            this.MSV.HeaderText = "MSV";
+            this.MSV.Name = "MSV";
+            this.MSV.ReadOnly = true;
+            // 
+            // Hoten
+            // 
+            this.Hoten.HeaderText = "Họ Tên";
+            this.Hoten.Name = "Hoten";
+            this.Hoten.ReadOnly = true;
+            this.Hoten.Width = 180;
+            // 
+            // lop
+            // 
+            this.lop.HeaderText = "Lớp";
+            this.lop.Name = "lop";
+            this.lop.ReadOnly = true;
+            this.lop.Width = 120;
+            // 
+            // Column1
+            // 
+            this.Column1.HeaderText = "Ngành";
+            this.Column1.Name = "Column1";
+            this.Column1.ReadOnly = true;
+            this.Column1.Width = 90;
+            // 
+            // button2
+            // 
+            this.button2.Cursor = System.Windows.Forms.Cursors.Default;
+            this.button2.Location = new System.Drawing.Point(16, 368);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(76, 39);
+            this.button2.TabIndex = 13;
+            this.button2.Text = "Thêm";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(216, 368);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(76, 39);
+            this.button3.TabIndex = 14;
+            this.button3.Text = "Xoá";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
             // frmQuanLyTaiKhoan
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(561, 416);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
             this.Controls.Add(this.label5);
             this.Controls.Add(this.textBox4);
             this.Controls.Add(this.textBox3);
             this.Controls.Add(this.textBox2);
             this.Controls.Add(this.textBox1);
-            this.Controls.Add(this.btnDoiMatKhau);
             this.Controls.Add(this.btnThoat);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.dataGridView1);
@@ -227,16 +246,18 @@
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Label label4;
         private System.Windows.Forms.DataGridView dataGridView1;
-        private System.Windows.Forms.DataGridViewTextBoxColumn MSV;
-        private System.Windows.Forms.DataGridViewTextBoxColumn Hoten;
-        private System.Windows.Forms.DataGridViewTextBoxColumn lop;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button btnThoat;
-        private System.Windows.Forms.Button btnDoiMatKhau;
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.TextBox textBox2;
         private System.Windows.Forms.TextBox textBox3;
         private System.Windows.Forms.TextBox textBox4;
         private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.DataGridViewTextBoxColumn MSV;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Hoten;
+        private System.Windows.Forms.DataGridViewTextBoxColumn lop;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
     }
 }
